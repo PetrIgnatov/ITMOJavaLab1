cac69ce15862b62b16ffd67660159962e5cfe60e
PetrIgnatov <144905118+PetrIgnatov@users.noreply.github.com>
2023-09-21 17:49:01 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -17,17 +17,17 @@ public class Main
     		{
       			for (int j = 0; j < 18; j++)
       			{
-        			if (c[i] == 4)
+        			switch (c[i])
         			{
-          				ans[i][j] = (1-Math.asin(Math.pow((x[j]-1)/8.0,2))/0.5);
-        			}
-        			else if (c[i] == 5 || c[i] == 6 || c[i] == 9 || c[i] == 11 || c[i] == 13 || c[i] == 15)
-        			{
-          				ans[i][j] = Math.pow((Math.exp(Math.atan((x[j]-1)/8.0))-1)/4.0,Math.asin(Math.sin(x[j])));
-        			}
-        			else
-        			{
-        				ans[i][j] = Math.cos(Math.tan(Math.sin(Math.asin((x[j]-1)/8.0))));
+					case 4:
+          					ans[i][j] = (1-Math.asin(Math.pow((x[j]-1)/8.0,2))/0.5);
+        					break;
+        				case 5, 6, 9, 11, 13, 15:
+          					ans[i][j] = Math.pow((Math.exp(Math.atan((x[j]-1)/8.0))-1)/4.0,Math.asin(Math.sin(x[j])));
+        					break;
+        				default:
+        					ans[i][j] = Math.cos(Math.tan(Math.sin(Math.asin((x[j]-1)/8.0))));
+						break;
         			}
         			System.out.printf("%5.2f ",ans[i][j]);
       			}
